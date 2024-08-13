e9c965af6479b57dfca1894c6984069805273fd8
Ilya Istomin <istominilya97@gmail.com>
2024-08-13 12:45:05 +0300

    initial commit

---
+++ src/__tests__/basic.js
@@ -0,0 +1,48 @@
+import { calculteTotal } from '../calculate.js';
+
+test('basic test', () => {
+  const result = 4;
+  expect(result).toBe(4);
+});
+
+test('calculateTotal sum', () => {
+  const list = [
+    {
+      id: 132,
+      name: 'война и мир',
+      count: 3,
+      price: 401,
+    },
+    {
+      id: 132,
+      name: 'JS',
+      count: 1,
+      price: 1300,
+    },
+
+  ];
+
+  const result = calculteTotal(list);
+  expect(result).toBe(2503);
+});
+
+test('calculateTotal sum with discount', () => {
+  const list = [
+    {
+      id: 132,
+      name: 'война и мир',
+      count: 3,
+      price: 400,
+    },
+    {
+      id: 132,
+      name: 'JS',
+      count: 1,
+      price: 1300,
+    },
+
+  ];
+
+  const result = calculteTotal(list, true);
+  expect(result).toBeCloseTo(2227.5);
+});
